da75f5fc9f0b4d7062daa12ef5810250a792eeba
Markus Vieghofer <markus.vieghofer@gmail.com>
2015-02-14 14:58:24 +0100

    added 'letter count' challenge

---
+++ medium/letter_count.py
@@ -0,0 +1,19 @@
+def LetterCount(str): 
+    words = str.split(" ")
+    result_word = ""
+    letter_count = 0
+    for word in words:
+        word_map = {}
+        for ch in word:
+            if ch in word_map:
+                word_map[ch] += 1
+            else:
+                word_map[ch] = 1
+        max_key = max(word_map.iterkeys(), key=lambda k: word_map[k])
+        if letter_count < word_map[max_key] and word_map[max_key] > 1:
+            letter_count = word_map[max_key]
+            result_word = word
+    return result_word if letter_count > 1 else -1
+ 
+print LetterCount("Hello apple pie")
+print LetterCount("No words")
